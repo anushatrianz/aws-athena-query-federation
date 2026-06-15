9a2f5eebc01b78789c1142c8a1e89ee55983d241
Anusha.NS <Anusha.NS@trianz.com>
2026-06-15 12:26:12 +0530

    Added logs

--- athena-dynamodb/src/main/java/com/amazonaws/athena/connectors/dynamodb/DynamoDBMetadataHandler.java
+++ athena-dynamodb/src/main/java/com/amazonaws/athena/connectors/dynamodb/DynamoDBMetadataHandler.java
@@ -78,8 +78,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.awscore.AwsRequestOverrideConfiguration;
 import software.amazon.awssdk.enhanced.dynamodb.document.EnhancedDocument;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.athena.AthenaClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.ExecuteStatementRequest;
 import software.amazon.awssdk.services.dynamodb.model.ExecuteStatementResponse;
@@ -90,6 +92,7 @@ import software.amazon.awssdk.services.glue.model.FederationSourceErrorCode;
 import software.amazon.awssdk.services.glue.model.Table;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -163,9 +166,7 @@ public class DynamoDBMetadataHandler
     public DynamoDBMetadataHandler(java.util.Map<String, String> configOptions)
     {
         super(SOURCE_TYPE, configOptions);
-        this.ddbClient = DynamoDbClient.builder() 
-                .credentialsProvider(CrossAccountCredentialsProviderV2.getCrossAccountCredentialsIfPresent(configOptions, "DynamoDBMetadataHandler_CrossAccountRoleSession"))
-                .build();
+        this.ddbClient = buildDynamoDbClient(configOptions);
         this.glueClient = getAwsGlue();
         this.invoker = ThrottlingInvoker.newDefaultBuilder(EXCEPTION_FILTER, configOptions).build();
         this.tableResolver = new DynamoDBTableResolver(invoker, ddbClient);
@@ -189,6 +190,18 @@ public class DynamoDBMetadataHandler
         this.invoker = ThrottlingInvoker.newDefaultBuilder(EXCEPTION_FILTER, configOptions).build();
         this.tableResolver = new DynamoDBTableResolver(invoker, ddbClient);
         this.queryPassthrough = new DDBQueryPassthrough();
+    }
+
+    private static DynamoDbClient buildDynamoDbClient(java.util.Map<String, String> configOptions)
+    {
+        String region = System.getenv("AWS_REGION");
+        DynamoDbClientBuilder builder = DynamoDbClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(CrossAccountCredentialsProviderV2.getCrossAccountCredentialsIfPresent(configOptions, "DynamoDBMetadataHandler_CrossAccountRoleSession"));
+        if (region != null && region.startsWith("eusc-")) {
+            builder.endpointOverride(URI.create("https://dynamodb." + region + ".amazonaws.eu"));
+        }
+        return builder.build();
     }
 
     @Override
@@ -275,6 +288,13 @@ public class DynamoDBMetadataHandler
         // add tables that may not be in Glue (if listing the default schema)
         if (DynamoDBConstants.DEFAULT_SCHEMA.equals(request.getSchemaName())) {
             FederatedIdentity federatedIdentity = request.getIdentity();
+            logger.info(
+                    "Identity Debug -> arn={}, account={}, tags={}, groups={}",
+                    federatedIdentity.getArn(),
+                    federatedIdentity.getAccount(),
+                    federatedIdentity.getPrincipalTags(),
+                    federatedIdentity.getIamGroups()
+            );
             AwsRequestOverrideConfiguration overrideConfig = getRequestOverrideConfig(federatedIdentity.getConfigOptions());
             DynamoDBPaginatedTables ddbPaginatedResponse = tableResolver.listTables(request.getNextToken(),
                     request.getPageSize(), overrideConfig);
@@ -298,6 +318,13 @@ public class DynamoDBMetadataHandler
         queryPassthrough.verify(request.getQueryPassthroughArguments());
         String partiQLStatement = request.getQueryPassthroughArguments().get(DDBQueryPassthrough.QUERY);
         FederatedIdentity federatedIdentity = request.getIdentity();
+        logger.info(
+                "Identity Debug -> arn={}, account={}, tags={}, groups={}",
+                federatedIdentity.getArn(),
+                federatedIdentity.getAccount(),
+                federatedIdentity.getPrincipalTags(),
+                federatedIdentity.getIamGroups()
+        );
         AwsRequestOverrideConfiguration overrideConfig = getRequestOverrideConfig(federatedIdentity.getConfigOptions());
         ExecuteStatementRequest executeStatementRequest =
                 ExecuteStatementRequest.builder()
@@ -335,6 +362,13 @@ public class DynamoDBMetadataHandler
             }
         }
         FederatedIdentity federatedIdentity = request.getIdentity();
+        logger.info(
+                "Identity Debug -> arn={}, account={}, tags={}, groups={}",
+                federatedIdentity.getArn(),
+                federatedIdentity.getAccount(),
+                federatedIdentity.getPrincipalTags(),
+                federatedIdentity.getIamGroups()
+        );
         AwsRequestOverrideConfiguration overrideConfig = getRequestOverrideConfig(federatedIdentity.getConfigOptions());
         // ignore database/schema name since there are no databases/schemas in DDB
         Schema schema = tableResolver.getTableSchema(request.getTableName().getTableName(), overrideConfig);
@@ -432,6 +466,13 @@ public class DynamoDBMetadataHandler
             throws Exception
     {
         FederatedIdentity federatedIdentity = request.getIdentity();
+        logger.info(
+                "Identity Debug -> arn={}, account={}, tags={}, groups={}",
+                federatedIdentity.getArn(),
+                federatedIdentity.getAccount(),
+                federatedIdentity.getPrincipalTags(),
+                federatedIdentity.getIamGroups()
+        );
         AwsRequestOverrideConfiguration overrideConfig = getRequestOverrideConfig(federatedIdentity.getConfigOptions());
         // TODO consider caching this repeated work in #enhancePartitionSchema
         // use the source table name from the schema if available (in case Glue table name != actual table name)
@@ -560,6 +601,14 @@ public class DynamoDBMetadataHandler
             return setupQueryPassthroughSplit(request);
         }
         FederatedIdentity federatedIdentity = request.getIdentity();
+        
+        logger.info(
+                "Identity Debug -> arn={}, account={}, tags={}, groups={}",
+                federatedIdentity.getArn(),
+                federatedIdentity.getAccount(),
+                federatedIdentity.getPrincipalTags(),
+                federatedIdentity.getIamGroups()
+        );
         AwsRequestOverrideConfiguration overrideConfig = getRequestOverrideConfig(federatedIdentity.getConfigOptions());
 
         int partitionContd = decodeContinuationToken(request);
